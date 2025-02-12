96482b013e41b715a7c70c5cb9813d80cca07cf7
Александра Шабалова <aleksandrasabalova@Aleksandras-MacBook-Air.local>
2025-02-13 01:50:38 +0700

    Сделал компонент button

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,16 +1,29 @@
 import './style.scss'
+import Icon from '../icon/Icon'
+import { ReactElement } from 'react'
 
 
 type TProps = {
     label: string
     isDisabled: boolean
-    size: 'small' | 'medium' | 'large' | 'xs'
+    size: 'large' | 'medium' | 'small' | 'xs'
+    variant: 'dark' | 'light' | 'superLight' |'danger' | 'lightDanger'
+    onClick: () => void
+    isRounded: boolean
+    isMini: boolean
+    prependIcon?: ReactElement
+    appendIcon?: ReactElement
 }
 
 export default function Button (props:TProps) {
     return (
-        <button disabled={props.isDisabled} className={`button--size-${props.size} button`}>
-            {props.label}
+        <button disabled={props.isDisabled}
+                onClick={props.onClick}
+                className={`button--size-${props.size} button--variant-${props.variant} ${props.isRounded ? 'button--rounded' : ''} ${props.isMini ? 'button--mini' : ''} button`}>
+                {props.prependIcon ? (<div className="button__icon">{props.prependIcon}</div>) : null}
+                <Icon icon={'plus'}></Icon>
+                {props.label}
+                <Icon icon={'arrow'}></Icon>
         </button>
     )
 }
